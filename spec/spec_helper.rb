e900c00511a736b0237f522dd8fcc84002dc2cfa
Ariel Thomas <Ariel.Thomas@gmail.com>
2013-02-01 05:54:18 -0600

    Fixes support for changeset's user in the notes model. Tests all passing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_base_class_for_anonymous_controllers = true
 
   config.include Devise::TestHelpers,           :type => :controller
   config.include IntegrationHelpers,            :type => :request
